31104fc8b713015ce66d4ef4b19458ee830d1b17
Daniel Justin <danieljmuleke@gmail.com>
2022-02-23 19:25:51 +0200

    Added HIS styles

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,7 @@ import "@ionic/vue/css/display.css";
 /* Theme variables */
 import "./theme/variables.css";
 import "./theme/custom.css";
+import "./theme/his-style.css";
 
 const app = createApp(App).use(IonicVue).use(router);
 
