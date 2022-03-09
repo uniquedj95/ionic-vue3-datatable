09ea8fe10289208cc9f985f34103cbac9b17b2ca
Daniel Justin <danieljmuleke@gmail.com>
2022-03-09 21:15:11 +0200

    Remove unnecessary stylesheet files

--- src/main.ts
+++ src/main.ts
@@ -23,8 +23,6 @@ import "@ionic/vue/css/display.css";
 
 /* Theme variables */
 import "./theme/variables.css";
-import "./theme/custom.css";
-import "./theme/his-style.css";
 import "./theme/datatable.css";
 
 const app = createApp(App).use(IonicVue).use(router);
